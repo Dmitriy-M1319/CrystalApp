bd7492089c98089e51683167344664840fd69021
Dmitriy-M1319 <dimon.5642014@yandex.ru>
2023-07-21 18:24:02 +0300

    add description for Swaggerpage

--- server/main.py
+++ server/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 
 from .routers import products, users, auth, cart, orders, admin
 from .db_engine import engine
 from .models import user, application, order, product
+
+
+def custom_docs():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="CrystalApp",
+        version="0.1.0",
+        summary="Документация OpenAPI для приложения CrystalApp",
+        description="Данная документация предоставляет API для информационной системы, представляющей собой интернет-магазин дляреализации фильтров для воды, а также менеджмент-систему склада и заказов пользователей для администраторов",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 
 user.Base.metadata.create_all(bind=engine)
@@ -16,5 +32,9 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
+
+app.openapi = custom_docs
+
+
 
 
