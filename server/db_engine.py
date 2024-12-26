b6e5603b6cfb6956a99cbd5691f81eec6b7a15d0
Dmitriy-M1319 <dimon.5642014@yandex.ru>
2024-12-26 10:58:49 +0300

    fix requirements

--- server/db_engine.py
+++ server/db_engine.py
@@ -1,11 +1,18 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./temp_db.sqlite3'
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+SQLALCHEMY_DATABASE_URL =  f"postgresql://{os.environ.get('DATABASE_USER')}:{os.environ.get('DATABASE_PASSWORD')}@{os.environ.get('DATABASE_IP')}:{os.environ.get('DATABASE_PORT')}/{os.environ.get('DATABASE_NAME')}"
+
+print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
-    # использование connect_args необходимо только для SQLite
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
